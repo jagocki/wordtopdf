afdbfcdfdd4e72a6a55c161bcef001ce2012a0c3
Adam Jagocki <adamjag@microsoft.com>
2019-03-13 14:18:26 +0100

    updates to references and minor conversion chancges

--- wordtopdf/Program.cs
+++ wordtopdf/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace wordtopdf
 {
@@ -10,6 +12,21 @@ namespace wordtopdf
     {
         static void Main(string[] args)
         {
+            Word.Application app = new Word.Application();
+
+            string[] docs = Directory.GetFiles(Environment.CurrentDirectory + @"\docs","*.docx",SearchOption.AllDirectories);
+
+            foreach(string docPath in docs)
+            {
+                var doc = app.Documents.Open(docPath);
+                string pdfPath = docPath.Replace("docx", "pdf");
+                doc.SaveAs2(pdfPath, Word.WdSaveFormat.wdFormatPDF);
+                doc.Close();
+                Console.WriteLine($"Converted {pdfPath}");
+                //use RAM disk? 
+                //https://blogs.technet.microsoft.com/windowsinternals/2017/08/25/how-to-create-a-ram-disk-in-windows-server/
+            }
+
         }
     }
 }
